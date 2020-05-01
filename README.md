058caffa629ca670dafcc5a9871a5a6df07ad90b
Nathan Merrill <mathiscool3000@gmail.com>
2020-05-01 18:15:06 +0000

    Add big triangle

---
+++ README.md
@@ -0,0 +1,2 @@
+# Covid 19
+A COVID-19 simulation written for a school project
---
+++ src/sketch.js
@@ -0,0 +1,7 @@
+export function setup() {
+    createCanvas(innerWidth, innerHeight);
+}
+
+export function draw() {
+    triangle(300, 500, 100, 20, 600, 130);
+}
